59803bc023275676c24503d9b69a3460ff17fb57
Elfling <tomasz@kominiarczuk.com>
2024-12-28 19:31:18 +0100

    Add ladder support and simple gravity for player

--- gameObjects/playerController.gd
+++ gameObjects/playerController.gd
@@ -8,7 +8,7 @@ enum Direction {
 }
 
 var actions = []
-var blockAction = false
+var blockAction = true
 @export var tilemap: TileMapLayer
 var turnManager
 
@@ -43,9 +43,15 @@ func getNextPosition(direction):
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	actions = []
-	blockAction = false
+	blockAction = true
+	print("ready player")
 	turnManager = get_node("/root/TurnManager")
-	
+	turnManager.onGravityCheck.connect(onGravityCheck)
+	turnManager.onTurnStart.connect(unblockActions)
+
+func unblockActions(turnIndex):
+	blockAction = false
+
 func startMove(direction):
 	if direction not in actions:
 		actions.append(direction)
@@ -53,25 +59,84 @@ func startMove(direction):
 func stopMove(direction):
 	if direction in actions:
 		actions.erase(direction)
-	if len(actions) < 1:
-		blockAction = false
+	
+func getPossibleActions():
+	var current = getCurrentPosition()
+	var currentCellData: TileData = tilemap.get_cell_tile_data(current)
+	var result = []
+	
+	for action in actions:
+		if action == Direction.left or action == Direction.right:
+			var positionUnderPlayer = getNextPosition(Direction.down)
+			var positionUnderCellData: TileData = tilemap.get_cell_tile_data(positionUnderPlayer)
+			if currentCellData and currentCellData.get_custom_data("verticalMoving") \
+			 or (not currentCellData or currentCellData.get_custom_data("empty")) \
+			 and positionUnderCellData and not positionUnderCellData.get_custom_data("empty"):
+				result.append(action)
+		if action == Direction.up:
+			var nextPosition = getNextPosition(action)
+			var nextCellData: TileData = tilemap.get_cell_tile_data(nextPosition)
+			if currentCellData and currentCellData.get_custom_data("verticalMoving") \
+			 and nextCellData and nextCellData.get_custom_data("verticalMoving"):
+				result.append(action)
+			
+		if action == Direction.down:
+			if currentCellData and currentCellData.get_custom_data("verticalMoving"):
+				result.append(action)
+
+	
+	return result
+	
+func isFalling():
+	var current = getCurrentPosition()
+	var currentCellData: TileData = tilemap.get_cell_tile_data(current)
+	
+	var positionUnderPlayer = getNextPosition(Direction.down)
+	var positionUnderCellData: TileData = tilemap.get_cell_tile_data(positionUnderPlayer)
+	
+	if currentCellData and currentCellData.get_custom_data("verticalMoving"):
+		return false
+	
+	if not positionUnderCellData:
+		return true
+	
+		
+	return positionUnderCellData.get_custom_data("empty")
 	
 func doAction(actions):
-	if len(actions) < 1:
-		return
+	var possibleActions = getPossibleActions()
 	if blockAction:
 		return
-	blockAction = true
-	var action = actions[0]
-
+		
+	if isFalling():
+		blockAction = true
+		turnManager.playerActionSelect.emit(tilemap)
+		return
+		
+	if len(possibleActions) < 1:
+		return
+	
+	
 	var current = getCurrentPosition()
+	var action = possibleActions[0]
 	var nextPosition = getNextPosition(action)
 	var nextCellData: TileData = tilemap.get_cell_tile_data(nextPosition)
-	
-	if nextCellData and nextCellData.get_custom_data("walkable"):
+	if not nextCellData or nextCellData.get_custom_data("empty"):
+		blockAction = true
 		position = position + getTransformVector(action)
 		turnManager.playerActionSelect.emit(tilemap)
 		return
+
+
+func onGravityCheck(tileMap):
+	if not isFalling():
+		return
+	var action = Direction.down
+	var nextPosition = getNextPosition(action)
+	var nextCellData: TileData = tileMap.get_cell_tile_data(nextPosition)
+	if not nextCellData or nextCellData.get_custom_data("empty"):
+		position = position + getTransformVector(action)
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- globals/TurnManager.gd
+++ globals/TurnManager.gd
@@ -3,23 +3,34 @@ extends Node
 var currentTurn = 0
 signal playerActionSelect(tileMap)
 signal onTurnChange(tileMap)
+signal onGravityCheck(tileMap)
 signal onTurnStart(currentTurnIndex)
 
 var gameStarted: bool = false
+var counter = 0
+var timeToSleep = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("_ready TimeManager")
 	playerActionSelect.connect(changeTurn)
+	counter = timeToSleep
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if gameStarted == false:
+	if counter < timeToSleep:
+		counter += delta
+	if gameStarted == false and counter >= timeToSleep:
+		print("start turn: " + str(counter))
 		gameStarted = true
+		
 		onTurnStart.emit(currentTurn)
 
 func changeTurn(tileMap):
 	onTurnChange.emit(tileMap)
+	onGravityCheck.emit(tileMap)
 	print("Turn " + str(currentTurn) + " has ended")
 	currentTurn += 1
-	onTurnStart.emit(currentTurn)
+	gameStarted = false
+	counter = 0
